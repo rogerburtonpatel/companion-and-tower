ed61d77885ef62be4ce79f96cadc840aa3f58590
Roger Burtonpatel <burtonpatel1@gmail.com>
2026-09-05 12:38:08 -0400

    Library only: drop paper, slides, paper-data, notes, and editor/agent config

--- paper-data/rocqwc-mwe/overcount.v
+++
@@ -1,5 +0,0 @@
-Require Import Foo.MyDefinition.
-
-Definition a := 0.
-Definition b := 1.
-Definition c := 2.
--- paper-data/rocqwc-mwe/undercount-control.v
+++
@@ -1,6 +0,0 @@
-Definition f (A : Type) (x : A) := x.
-
-Instance foo (y : nat) : f nat 0 = 0.
-Proof.
-  reflexivity.
-Qed.
